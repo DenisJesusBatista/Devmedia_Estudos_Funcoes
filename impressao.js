882e5c58ad5fe0e14546b1d6c2b2f17f79aebb41
Denis Batista <denis.si2011@gmail.com>
2022-04-19 09:45:22 -0300

    FIX: Treinando função.

---
+++ impressao.js
@@ -0,0 +1,3 @@
+console.log(5 + 10 * 3);
+console.log(1 + true);
+console.log(5 + undefined);
---
+++ media.js
@@ -0,0 +1,19 @@
+let numeros = [1, 2, 5, 8, 14];
+
+const calcula_media = (numeros) => {
+
+    let total_numeros = numeros.length;
+    let soma_numeros = 0;
+
+    for (const numeros of numero) {
+        soma_numeros = soma_numeros + numero;
+    }
+
+    let media = soma_numeros / total_numeros;
+
+    return media;
+}
+
+let media = calcula_media(numeros);
+
+console.log("A média dos números é: " + media);
