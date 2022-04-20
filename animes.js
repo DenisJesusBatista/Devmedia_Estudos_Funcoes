7dfa83fcaad257299692782ac1362fb00fb92f45
Denis Batista <denis.si2011@gmail.com>
2022-04-20 12:16:48 -0300

    FIX: Nota aluno.

---
+++ animes.js
@@ -0,0 +1,11 @@
+let imprime_itens = () => {
+
+    for (const anime of animes) {
+        console.log(anime);
+    }
+
+}
+
+let animes = ['Samurai X', 'Naruto', 'Death Note', 'Tokyo Ghoul', 'Bleach'];
+
+imprime_itens(animes);
